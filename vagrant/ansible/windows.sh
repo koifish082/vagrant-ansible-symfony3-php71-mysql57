273d2ee5363f62ee9b6338dc3c5a6477f9419376
koifish082 <ganbare.carp@gmail.com>
2017-03-17 00:26:48 +0900

    Fix roles for Ansible2.2

--- vagrant/ansible/windows.sh
+++ vagrant/ansible/windows.sh
@@ -3,24 +3,9 @@
 # Update Repositories
 sudo apt-get update
 
-# Determine Ubuntu Version
-. /etc/lsb-release
-
-# Decide on package to install for `add-apt-repository` command
-#
-# USE_COMMON=1 when using a distribution over 12.04
-# USE_COMMON=0 when using a distribution at 12.04 or older
-USE_COMMON=$(echo "$DISTRIB_RELEASE > 12.04" | bc)
-
-if [ "$USE_COMMON" -eq "1" ];
-then
-    sudo apt-get install -y software-properties-common
-else
-    sudo apt-get install -y python-software-properties
-fi
-
 # Add Ansible Repository & Install Ansible
-sudo add-apt-repository -y ppa:ansible/ansible
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update
 sudo apt-get install -y ansible
 
